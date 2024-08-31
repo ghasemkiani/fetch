73d2b0329110c3d7c355748b0c642e254b45de2a
Ghasem Kiani <ghasemkiani@gmail.com>
2024-08-31 23:50:02 +0330

    update

--- worker.js
+++ worker.js
@@ -1,6 +1,8 @@
 export default {
   async fetch(request, env, ctx) {
-    const { url, options } = JSON.parse(new URL(request.url).searchParams.get("data"));
+    const { url, options } = JSON.parse(
+      new URL(request.url).searchParams.get("data")
+    );
     const response = await fetch(url, options);
     const { readable, writable } = new TransformStream();
     response.body.pipeTo(writable);
